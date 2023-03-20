2543a09b0350cfb198028564f46a832b545f49df
Michele Francesco Meloni <michele.f.meloni@avanade.com>
2023-03-20 17:52:06 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace DataBaseEntityFrameworkNO_CORE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btn_Load = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 166);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(887, 393);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(30, 56);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(347, 28);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // btn_Load
+            // 
+            this.btn_Load.Location = new System.Drawing.Point(421, 44);
+            this.btn_Load.Name = "btn_Load";
+            this.btn_Load.Size = new System.Drawing.Size(243, 69);
+            this.btn_Load.TabIndex = 2;
+            this.btn_Load.Text = "Load Table";
+            this.btn_Load.UseVisualStyleBackColor = true;
+            this.btn_Load.Click += new System.EventHandler(this.btn_Load_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(935, 256);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 188);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Salva Aggiornamento";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(729, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 40);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1095, 602);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_Load);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btn_Load;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Infrastructure;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity;
+using System.Net.Http.Headers;
+using DataBaseEntityFrameworkNO_CORE;
+
+namespace DataBaseEntityFrameworkNO_CORE
+{
+    public partial class Form1 : Form
+    {
+        private AcademyNetEntities1 cxt;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cxt = new AcademyNetEntities1();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            comboBox1.DataSource = GetNameTables();
+
+        }
+
+        private List<string> GetNameTables()
+        {
+            List<string> result = new List<string>();
+            var metadata = ((IObjectContextAdapter)cxt).ObjectContext.MetadataWorkspace;    //info sul db
+
+            //prende le entità indicate come table e le mette in table
+            var tables = metadata.GetItemCollection(DataSpace.SSpace)
+                .GetItems<EntityContainer>()
+                .Single()
+                .BaseEntitySets
+                .OfType<EntitySet>()
+                .Where(s => !s.MetadataProperties.Contains("Type")
+                || s.MetadataProperties["Type"].ToString() == "Tables");
+
+            foreach (var table in tables)
+            {
+                var tableName = table.MetadataProperties.Contains("Table")
+                   && table.MetadataProperties["Table"].Value != null
+                    ? table.MetadataProperties["Table"].Value.ToString()
+                    : table.Name;
+
+                var tableSchema = table.MetadataProperties["Schema"].Value.ToString();
+
+                result.Add(tableName);
+
+            }
+            return result;
+        }
+
+        //private List<EntitySet> GetDataTable()
+        //{
+        //    List<EntitySet> dtList = new List<EntitySet>();
+        //    using (cxt)
+        //    {
+        //        var metadata = ((IObjectContextAdapter)cxt).ObjectContext.MetadataWorkspace;
+
+        //        var tables = metadata.GetItemCollection(DataSpace.SSpace)
+        //                       .GetItems<EntityContainer>()
+        //                       .Single()
+        //                       .BaseEntitySets
+        //                       .OfType<EntitySet>()
+        //                       .Where(s => !s.MetadataProperties.Contains("Type")
+        //                         || s.MetadataProperties["Type"].ToString() == "Tables");
+        //        foreach (var table in tables)
+        //        {
+        //            dtList.Add(table);
+        //        }
+        //    }
+
+
+        //    return dtList;
+        //}
+        //private List<object> GetObject()
+        //{
+        //    List<object> result = new List<object>();
+        //    var metadata = ((IObjectContextAdapter)cxt).ObjectContext.MetadataWorkspace;    //info sul db
+
+        //    //prende le entità indicate come table e le mette in table
+        //    var tables = metadata.GetItemCollection(DataSpace.SSpace)
+        //        .GetItems<EntityContainer>()
+        //        .Single()
+        //        .BaseEntitySets
+        //        .OfType<EntitySet>()
+        //        .Where(s => !s.MetadataProperties.Contains("Type")
+        //        || s.MetadataProperties["Type"].ToString() == "Tables");
+
+        //    foreach (var table in tables)
+        //    {
+        //        var tableName = table.MetadataProperties.Contains("Table")
+        //           && table.MetadataProperties["Table"].Value != null
+        //            ? table.MetadataProperties["Table"].Value
+        //            : table.Name;
+
+        //        result.Add(tableName);
+
+        //    }
+        //    return result;
+        //}
+
+
+        //public object GetSet(AcademyNetEntities1 nome, string nomeTable)
+        //{
+        //    switch (nomeTable)
+        //    {
+        //        case "staff":
+        //            {
+        //                return nome.staffs;
+        //            }
+        //        case "brands":
+        //            {
+        //                return nome.brands;
+        //            }
+        //        case "categories":
+        //            {
+        //                return nome.categories;
+        //            }
+        //        case "products":
+        //            {
+        //                return nome.products;
+        //            }
+        //        case "stocks":
+        //            {
+        //                return nome.stocks;
+        //            }
+        //        case "customers":
+        //            {
+        //                return nome.customers;
+        //            }
+        //        case "order_items":
+        //            {
+        //                return nome.order_items;
+        //            }
+        //        case "orders":
+        //            {
+        //                return nome.orders;
+        //            }
+        //        case "staffs":
+        //            {
+        //                return nome.staffs;
+        //            }
+        //        case "stores":
+        //            {
+        //                return nome.stores;
+        //            }
+        //        default:
+        //            {
+        //                return nome.brands;
+        //            }
+        //    }
+
+        //}
+
+        //private List<EntitySet> GetEntity()
+        //{
+        //    List<EntitySet> result = new List<EntitySet>();
+
+        //    using (cxt)
+        //    {
+        //        var metadata = ((IObjectContextAdapter)cxt).ObjectContext.MetadataWorkspace;
+
+        //        var tables = metadata.GetItemCollection(DataSpace.SSpace)
+        //                       .GetItems<EntityContainer>()
+        //                       .Single()
+        //                       .BaseEntitySets
+        //                       .OfType<EntitySet>()
+        //                       .Where(s => !s.MetadataProperties.Contains("Type")
+        //                         || s.MetadataProperties["Type"].ToString() == "Tables");
+
+        //        foreach (var table in tables)
+        //        {
+        //            result.Add(table);
+
+        //        }
+        //        return result;
+        //    }
+        //}
+
+        //public List<string> GetTables()
+        //{
+        //    List<string> result = new List<string>();
+
+        //    using (cxt)
+        //    {
+        //        var metadata = ((IObjectContextAdapter)cxt).ObjectContext.MetadataWorkspace;
+
+        //        var tables = metadata.GetItemCollection(DataSpace.SSpace)
+        //                       .GetItems<EntityContainer>()
+        //                       .Single()
+        //                       .BaseEntitySets
+        //                       .OfType<EntitySet>()
+        //                       .Where(s => !s.MetadataProperties.Contains("Type")
+        //                         || s.MetadataProperties["Type"].ToString() == "Tables");
+
+        //        foreach (var table in tables)
+        //        {
+        //            var tableName = table.MetadataProperties.Contains("Table")
+        //                           && table.MetadataProperties["Table"].Value != null
+        //                         ? table.MetadataProperties["Table"].Value.ToString()
+        //                         : table.Name;
+
+        //            var tableSchema = table.MetadataProperties["Schema"].Value.ToString();
+        //            if (tableName == "sysdiagrams")
+        //            {
+        //                continue;
+        //            }
+        //            else
+        //            {
+        //                result.Add(tableName);
+        //            }
+        //        }
+        //        return result;
+        //    }
+        //}
+
+        private void btn_Load_Click(object sender, EventArgs e)
+        {
+            var selezione = comboBox1.SelectedItem;
+            switch (selezione)
+            {
+                case "staffs":
+
+                    dataGridView1.DataSource = cxt.staffs.ToList();
+                    break;
+                case "brands":
+
+                    dataGridView1.DataSource = cxt.brands.ToList();
+                    break;
+
+                case "categories":
+
+                    dataGridView1.DataSource = cxt.categories.ToList();
+                    break;
+
+                case "products":
+
+                    dataGridView1.DataSource = cxt.products.ToList();
+                    break;
+
+                case "stocks":
+
+                    dataGridView1.DataSource = cxt.stocks.ToList();
+                    break;
+
+                case "customers":
+
+                    dataGridView1.DataSource = cxt.customers.ToList();
+                    break;
+
+                case "order_items":
+
+                    dataGridView1.DataSource = cxt.order_items.ToList();
+                    break;
+
+                case "orders":
+
+                    dataGridView1.DataSource = cxt.orders.ToList();
+                    break;
+
+                default:
+
+                    dataGridView1.DataSource = cxt.stores.ToList();
+                    break;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cxt.SaveChanges();
+            MessageBox.Show("Dati aggiornati!");
+        }
+    }
+}
